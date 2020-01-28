d69a379ec87dec42e04fba7081c6e87d44ae0629
Wim von Benecke <boneswvb@gmail.com>
2020-01-28 04:56:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css'
+
+import Header from "./Header"
+import Button from "./Button"
+
+function App() {
+    return (
+        <div className="tc">
+            <Header />
+            <Button />
+        </div>
+    )
+}
+
+export default App
