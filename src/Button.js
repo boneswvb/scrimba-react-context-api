bbd8b1aaf790c453a8902409a448690e19ef151f
Wim von Benecke <boneswvb@gmail.com>
2020-01-30 04:19:29 +0200

    update 30/01/2020 - part one

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react"
-// import ThemeContext from "./themeContext"
+import ThemeContext from "./themeContext"
 
 class Button extends Component {
     render() {
+    	const theme = this.context
         return (
-            <button className="light-theme">Switch Theme</button>
+            <button className={`${ theme }-theme` }>Switch Theme</button>
         )    
     }
 }
 
+Button.contextType = ThemeContext
 export default Button
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ThemeContext from './themeContext';
 import App from './App';
 import'tachyons'
 import * as serviceWorker from './serviceWorker';
 
-const ThemeContext = React.createContext()
 // ThemeContext.Provider & ThemeContext.Consumer
 
 ReactDOM.render(
-    <ThemeContext.Provider>
-        <App />
-    </ThemeContext.Provider>,
-    document.getElementById("root")
+	<ThemeContext.Provider value={ "light" }>
+		<App />
+	</ThemeContext.Provider>
+	,document.getElementById("root")
 )
 
 // If you want your app to work offline and load faster, you can change
